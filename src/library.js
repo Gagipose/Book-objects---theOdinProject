c3722af899a1c293daea94cb3bfaa20016339ab4
David Lindblom <lindblom.david@hotmail.com>
2025-11-13 23:42:06 +0100

    exported and renamed files

---
+++ src/library.js
@@ -0,0 +1,50 @@
+// start with pseudo code
+
+// user gives title, author and pages
+// Fucntion makes book object named with unique id + added to array
+
+// so website can remember library
+export const myLibrary = [];
+
+
+// input: title, author, pages -> output: book object (with random id)
+export function Book(title, author, pages) {
+    if (!new.target) {
+        throw Error("gotta use the -new- keyword first");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.id = crypto.randomUUID();
+
+    
+};
+
+// makes adding unique books to array easier
+export function addBookToLibrary(title, author, pages) { 
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+};
+
+// **change so it uses unique id later**
+export function removeBookFromLibrary(title) {
+    const selectByTitle = myLibrary.findIndex(book => book.title === title)
+    myLibrary.splice(selectByTitle, 1);
+}
+
+
+
+// ---- FOR TESTING: ----
+addBookToLibrary("cool title", "kalle anka", 170);
+addBookToLibrary("boomzi", "honung Jr", 70);
+addBookToLibrary("321", "honung Jr", 70);
+
+
+// const testFilter = myLibrary.filter(book => book.pages === 70);
+// console.log(testFilter);
+
+// removeBookFromLibrary("boomzi")
+
+// console.log(myLibrary);
+
+export const mango = 123
--- src/script.js
+++ src/script.js
@@ -1,44 +1,8 @@
-// start with pseudo code
-
-// user gives title, author and pages
-// Fucntion makes book object named with unique id + added to array
+import * as library from "./library.js";
 
 
+library.addBookToLibrary("testerino", "bananski Jr", 70);
 
-const myLibrary = [];
+const testFilter = library.myLibrary.filter(book => book.title === "testerino")
 
-// input: title, author, pages -> output: book object (with random id)
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.id = crypto.randomUUID();
-
-    
-};
-
-// makes adding unique books to array easier
-function addBookToLibrary(title, author, pages) {
-    
-    const newBook = new Book(title, author, pages);
-    myLibrary.push(newBook);
-    
-};
-
-// **change so it uses unique id later**
-function removeBookFromLibrary(title) {
-    const selectByTitle = myLibrary.findIndex(book => book.title === title)
-    myLibrary.splice(selectByTitle, 1);
-}
-
-addBookToLibrary("cool title", "kalle anka", 170);
-addBookToLibrary("boomzi", "honung Jr", 70);
-addBookToLibrary("321", "honung Jr", 70);
-
-
-// const testFilter = myLibrary.filter(book => book.pages === 70);
-// console.log(testFilter);
-
-removeBookFromLibrary("boomzi")
-
-console.log(myLibrary);
+console.log(testFilter);
